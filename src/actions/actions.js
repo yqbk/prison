af3e3400133901c8a57ce8918957c40dfeaaabb6
Jakub Syrek <jksyrek@gmail.com>
2018-05-26 18:49:20 +0200

    Keep current index in state

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,16 +1,27 @@
 let URL = 'http://demo6315956.mockable.io';
 
 let actions = store => ({
-  // Async actions can be pure async/promise functions:
-  async getPeople(state) {
-    let res = await fetch(`${URL}/people`);
-    return { people: await res.json() };
-  },
+    // Async actions can be pure async/promise functions:
+    async getPeople(state) {
+        let res = await fetch(`${URL}/people`);
+        return { people: await res.json() };
+    },
 
-  async getMatches(state) {
-    let res = await fetch(`${URL}/matches`);
-    return { matches: await res.json() };
-  }
+    async getMatches(state) {
+        let res = await fetch(`${URL}/matches`);
+        return { matches: await res.json() };
+    },
+
+    setCurrentIndex: (state, index) => {
+        console.log('ustawiam index', index);
+        return { currentIndex: index };
+    },
+
+    removeCard: state => {
+        console.log('usuwam karte', state.people.length);
+
+        return { ...state, people: state.people.slice(1) };
+    }
 });
 
 export default actions;
