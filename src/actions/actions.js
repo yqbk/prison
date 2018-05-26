3bb85da39a41affd0c2f9b1e3b3087a7c1dccd17
Piotr Janosz <piotr@bakkenbaeck.no>
2018-05-26 21:31:54 +0200

    Cleanup

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,67 +1,64 @@
 let URL = 'http://demo9367428.mockable.io';
 
 let actions = store => ({
-    // Async actions can be pure async/promise functions:
-    async getPeople(state) {
-        let res = await fetch(`${URL}/people`);
-        return state.people.length ? { people: state.people } : { people: await res.json() };
-    },
+  // Async actions can be pure async/promise functions:
+  async getPeople(state) {
+    let res = await fetch(`${URL}/people`);
+    return state.people.length
+      ? { people: state.people }
+      : { people: await res.json() };
+  },
 
-    async getMatches(state) {
-        let res = await fetch(`${URL}/matches`);
-        return state.matches.length ? state.matches : { matches: await res.json() };
-    },
+  async getMatches(state) {
+    let res = await fetch(`${URL}/matches`);
+    return state.matches.length ? state.matches : { matches: await res.json() };
+  },
 
-    async getMessages(state) {
-        let res = await fetch(`${URL}/messages`);
-        return { messages: await res.json() };
-    },
+  async getMessages(state) {
+    let res = await fetch(`${URL}/messages`);
+    return { messages: await res.json() };
+  },
 
-    setCurrentIndex: (state, index) => {
-        return { ...state, currentIndex: index };
-    },
+  setCurrentIndex: (state, index) => {
+    return { ...state, currentIndex: index };
+  },
 
-    addMessage: (state, message) => {
-        return {
-            ...state,
-            totalMessages: state.totalMessages + 1,
-            messages: [...state.messages, message]
-        };
-    },
-    setCurrentIndex: (state, index) => {
-        return { ...state, currentIndex: index };
-    },
+  addMessage: (state, message) => {
+    return {
+      ...state,
+      totalMessages: state.totalMessages + 1,
+      messages: [...state.messages, message]
+    };
+  },
+  setCurrentIndex: (state, index) => {
+    return { ...state, currentIndex: index };
+  },
 
-    like: (state, id) => {
-        console.log('Like!');
-        const result = {
-            ...state,
-            matches: [...state.matches, state.people.find(prisoner => prisoner.id === id)],
-            people: [...state.people.filter(prisoner => prisoner.id !== id)],
-            // currentIndex: state.people[state.currentIndex + 1].id
-            currentIndex: state.currentIndex + 1
-            // currentIndex: state.people
-            //     .map((prisoner, index) => prisoner.id)
-            //     .slice(state.setCurrentIndex, state.currentIndex + 1)
-            //     .pop()
-        };
+  like: (state, id) => {
+    const result = {
+      ...state,
+      matches: [
+        ...state.matches,
+        state.people.find(prisoner => prisoner.id === id)
+      ],
+      people: [...state.people.filter(prisoner => prisoner.id !== id)],
+      currentIndex: state.currentIndex + 1
+    };
 
-        console.log('teest', id, result.currentIndex);
+    return result;
+  },
 
-        return result;
-    },
+  dislike: (state, id) => {
+    return {
+      ...state,
+      people: [...state.people.filter(prisoner => prisoner.id !== id)],
+      currentIndex: state.currentIndex + 1
+    };
+  },
 
-    dislike: (state, id) => {
-        return {
-            ...state,
-            people: [...state.people.filter(prisoner => prisoner.id !== id)],
-            currentIndex: state.currentIndex + 1
-        };
-    },
-
-    removeCard: state => {
-        return { ...state, people: state.people.slice(1) };
-    }
+  removeCard: state => {
+    return { ...state, people: state.people.slice(1) };
+  }
 });
 
 export default actions;
