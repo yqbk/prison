e7a509887ed0985836dacc3803c69b83215ff837
Jakub Syrek <jksyrek@gmail.com>
2018-05-26 17:01:16 +0200

    Color changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ import '../styles/footer.css';
 export default () => (
     <footer className="flex flex--aic flex--jcc">
         <button className="btn btn--round">🙅‍♂️</button>
-        <button className="btn btn--info">ℹ</button>
+        <button className="btn btn--info">ℹ️</button>
         <button className="btn btn--round" onClick={() => console.log('test')}>
             ❤️
         </button>
