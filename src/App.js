e7f075b1a27eb56b93dc98992385ec24b2e622a3
Jakub Syrek <jksyrek@gmail.com>
2018-05-26 12:21:35 +0200

    Add Swiper

--- src/App.js
+++ src/App.js
@@ -1,9 +1,48 @@
 import React, { Component, Fragment } from 'react';
+import Swiper from 'react-native-deck-swiper';
+import styled from 'styled-components';
 
 class App extends Component {
-  render() {
-    return <Fragment>Hello world</Fragment>;
-  }
+    constructor() {
+        super(props);
+
+        this.state = {};
+    }
+    render() {
+        return (
+            <Fragment>
+                {' '}
+                <Swiper
+                    cards={['DO', 'MORE', 'OF', 'WHAT', 'MAKES', 'YOU', 'HAPPY']}
+                    renderCard={card => {
+                        return (
+                            <View style={styles.card}>
+                                <Text style={styles.text}>{card}</Text>
+                            </View>
+                        );
+                    }}
+                    onSwiped={cardIndex => {
+                        console.log(cardIndex);
+                    }}
+                    onSwipedAll={() => {
+                        console.log('onSwipedAll');
+                    }}
+                    cardIndex={0}
+                    backgroundColor={'#4FD0E9'}
+                    stackSize={3}
+                >
+                    <Button
+                        onPress={() => {
+                            console.log('oulala');
+                        }}
+                        title="Press me"
+                    >
+                        You can press me
+                    </Button>
+                </Swiper>
+            </Fragment>
+        );
+    }
 }
 
 export default App;
