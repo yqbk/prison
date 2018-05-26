7db88783ad69780d517d5491603715adbb3ed110
Piotr Janosz <piotr@bakkenbaeck.no>
2018-05-26 20:55:13 +0200

    Added AI

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,50 +1,56 @@
 let URL = 'http://demo9367428.mockable.io';
 
 let actions = store => ({
-    // Async actions can be pure async/promise functions:
-    async getPeople(state) {
-        let res = await fetch(`${URL}/people`);
-        return state.people.length ? { people: state.people } : { people: await res.json() };
-    },
+  // Async actions can be pure async/promise functions:
+  async getPeople(state) {
+    let res = await fetch(`${URL}/people`);
+    return state.people.length
+      ? { people: state.people }
+      : { people: await res.json() };
+  },
 
-    async getMatches(state) {
-        let res = await fetch(`${URL}/matches`);
-        return state.matches.length ? state.matches : { matches: await res.json() };
-    },
+  async getMatches(state) {
+    let res = await fetch(`${URL}/matches`);
+    return state.matches.length ? state.matches : { matches: await res.json() };
+  },
 
-    async getMessages(state) {
-        let res = await fetch(`${URL}/messages`);
-        return { messages: await res.json() };
-    },
+  async getMessages(state) {
+    let res = await fetch(`${URL}/messages`);
+    return { messages: await res.json() };
+  },
 
-    setCurrentIndex: (state, index) => {
-        return { ...state, currentIndex: index };
-    },
+  setCurrentIndex: (state, index) => {
+    return { ...state, currentIndex: index };
+  },
 
-    removeCard: state => {
-        return { ...state, people: state.people.slice(1) };
-    },
+  removeCard: state => {
+    return { ...state, people: state.people.slice(1) };
+  },
 
-    insertMessage: state => {
-        return {
-            ...state,
-            totalMessages: state.totalMessages + 1
-        };
-    },
-    setCurrentIndex: (state, index) => {
-        return { ...state, currentIndex: index };
-    },
+  addMessage: (state, message) => {
+    return {
+      ...state,
+      totalMessages: state.totalMessages + 1,
+      messages: [...state.messages, message]
+    };
+  },
+  setCurrentIndex: (state, index) => {
+    return { ...state, currentIndex: index };
+  },
 
-    like: (state, id) => {
-        return {
-            ...state,
-            matches: [...state.matches, state.people.find(prisoner => prisoner.id === id)]
-        };
-    },
+  like: (state, id) => {
+    return {
+      ...state,
+      matches: [
+        ...state.matches,
+        state.people.find(prisoner => prisoner.id === id)
+      ]
+    };
+  },
 
-    removeCard: state => {
-        return { ...state, people: state.people.slice(1) };
-    }
+  removeCard: state => {
+    return { ...state, people: state.people.slice(1) };
+  }
 });
 
 export default actions;
